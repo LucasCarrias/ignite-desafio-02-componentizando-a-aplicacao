ccce437b312aefa49d505b96f4328af3c1ef4da0
Lucas Carrias <lucascarrias@outlook.com>
2021-05-05 10:17:30 -0300

    Create component SideBar

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,36 @@
-export function SideBar() {
-  // Complete aqui
+import React from "react";
+import { Button } from "./Button";
+
+interface Genre {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+type SideBarProps = {
+  selectedGenreId: number,
+  genres: Genre[],
+  handleClickButton: (id: number) => void
+}
+
+export function SideBar({ genres, selectedGenreId, handleClickButton }: SideBarProps) {
+  return (
+    <nav className="sidebar">
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className="buttons-container">
+        {genres.map((genre) => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
 }
