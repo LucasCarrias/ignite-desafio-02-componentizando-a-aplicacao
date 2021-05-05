9a9db381710ac110e7f4336ad020cd4fcb1e732a
Lucas Carrias <lucascarrias@outlook.com>
2021-05-05 11:07:13 -0300

    Create Content component

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,22 @@
-export function Content() {
-  // Complete aqui
+import Genre from "../interfaces/genre";
+import Movie from "../interfaces/movie";
+import { MoviesList } from "./MoviesList";
+
+interface ContentProps {
+  movies: Movie[];
+  selectedGenre: Genre;
+}
+
+export function Content({movies, selectedGenre}: ContentProps) {
+  return (
+    <div className="container">
+      <header>
+        <span className="category">
+          Categoria:<span> {selectedGenre.title}</span>
+        </span>
+      </header>
+
+      <MoviesList movies={movies}/>
+    </div>
+  );
 }
